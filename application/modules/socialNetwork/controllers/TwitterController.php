9a65ede18c16dd36329a039d68b46f46e49af17a
rachit-doshi <doshi.rachit@gmail.com>
2013-12-22 02:44:20 +0530

    Routing via Ajax Calls
    Tested Routing Via Ajax Calls for Url :
    http://web-tools.local/social/facebook_like

--- application/modules/socialNetwork/controllers/TwitterController.php
+++ application/modules/socialNetwork/controllers/TwitterController.php
@@ -6,7 +6,12 @@ class SocialNetwork_TwitterController extends Zend_Controller_Action {
      * @url http://web-tools.local/social/twitter_follow
      */
     public function followAction() {
-        
+        $request = $this->getRequest();
+        $data = $request->getParams();
+        echo '<pre>data == ';
+        print_r($data);
+        echo '</pre>';
+        exit;
     }
 
 }
